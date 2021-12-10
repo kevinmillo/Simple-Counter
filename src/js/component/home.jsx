66019a33b025d3a93f299a227205992107a0cb1c
kevinmillo <kbullor@hotmail.com>
2021-12-10 16:02:46 +0000

    entrega simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contador">
+			<div className="segunderos">
+				<div className="reloj">
+					<i class="far fa-clock"></i>
+				</div>
+				<div className="digito">{props.hora1}</div>
+				<div className="digito">{props.hora0}</div>
+				<div className="digito">{props.minuto1}</div>
+				<div className="digito">{props.minuto0}</div>
+				<div className="digito">{props.segundo1}</div>
+				<div className="digito">{props.segundo0}</div>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,46 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let segundo0 = 0;
+let segundo1 = 0;
+let minuto0 = 0;
+let minuto1 = 0;
+let hora0 = 0;
+let hora1 = 0;
+setInterval(() => {
+	segundo0++;
+	if (segundo0 > 9) {
+		segundo0 = 0;
+		segundo1++;
+		if (segundo1 > 5) {
+			segundo1 = 0;
+			minuto0++;
+			if (minuto0 > 9) {
+				minuto0 = 0;
+				minuto1++;
+				if (minuto1 > 5) {
+					minuto1 = 0;
+					hora0++;
+					if (hora0 > 9) {
+						hora0 = 0;
+						hora1++;
+						if (hora1 > 9) {
+							hora1 = 0;
+						}
+					}
+				}
+			}
+		}
+	}
+	ReactDOM.render(
+		<Home
+			segundo0={segundo0}
+			segundo1={segundo1}
+			minuto0={minuto0}
+			minuto1={minuto1}
+			hora0={hora0}
+			hora1={hora1}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
